2defd790ba5ae0eefa97f2661c93257a9391c7ec
Rhys Tumelty <rhys@tumelty.co.uk>
2021-12-18 07:30:43 +0000

    Code

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-import flask
+from flask import Flask, send_from_directory, render_template, url_for, redirect
+import os
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return redirect(url_for('static', filename='favicon.ico'))
 
 @app.route("/")
 def index():
-    return "hello, world"
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000, debug=True)
